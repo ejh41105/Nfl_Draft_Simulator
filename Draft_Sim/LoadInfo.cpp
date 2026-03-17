1b5d38942dabd46fd25099ad91429cc9d7c01184
ejh41155 <ethanhoward411@gmail.com>
2026-03-16 23:36:59 -0500

    implemented input loader for players and teams, and created tests to ensure they properly work

---
+++ Draft_Sim/LoadInfo.cpp
@@ -0,0 +1,86 @@
+#include "LoadInfo.h"
+#include "json.hpp"
+#include <fstream>
+#include <iostream>
+
+using json = nlohmann::json;
+
+std::vector<Player> draftPool;
+std::vector<Team>   teamList;
+std::vector<Pick>   draftOrder;
+
+void loadPlayers(const std::string& path)
+{
+    std::ifstream file(path);
+
+    if (!file.is_open())
+    {
+        std::cerr << "Error: Could not open " << path << "\n";
+        return;
+    }
+
+    json data = json::parse(file);
+
+    for (const auto& p : data) {
+        draftPool.emplace_back(
+            p["name"].get<std::string>(),
+            p["position"].get<std::string>(),
+            p["college"].get<std::string>(),
+            p["majorStats"].is_null() ? "No stats available" : p["majorStats"].get<std::string>(),
+            p["number"].get<int>(),
+            p["age"].get<int>(),
+            p["height"].get<int>(),
+            p["weight"].get<int>(),
+            p["consensusRanking"].get<int>(),
+            p["positionalRanking"].get<int>(),
+            p["RAS"].get<double>(),
+            p["miscConcern"].get<int>(),
+            p["floorCeiling"].get<int>()
+        );
+    }
+}
+
+void loadTeams(const std::string& path)
+{
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: Could not open " << path << "\n";
+        return;
+    }
+
+    json data = json::parse(file);
+
+    for (const auto& t : data)
+    {
+        std::vector<Pick> picks;
+        for (const auto& pk : t["picks"])
+        {
+            picks.push_back({
+                pk["overall"].get<int>(),
+                pk["round"].get<int>(),
+                pk["selection"].get<int>()
+            });
+        }
+        std::vector<std::string> priorities = t["priorities"].get<std::vector<std::string>>();
+
+        teamList.emplace_back
+        (
+            t["id"].get<std::string>(),
+            t["name"].get<std::string>(),
+            t["city"].get<std::string>(),
+            picks,
+            t["positionalNeed"].get<std::map<std::string, int>>(),
+            t["noiseRange"].get<int>(),
+            priorities
+        );
+    }
+
+}
+
+void loadDraftOrder(const std::string& path)
+{
+
+}
+
+
